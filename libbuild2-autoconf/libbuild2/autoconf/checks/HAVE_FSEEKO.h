52bc2fafdc4939d13e69d34c5bfff98eb425947c
Francois Kritzinger <francoisk777@gmail.com>
2025-05-13 15:05:45 +0200

    Update HAVE_FSEEKO
    Update FreeBSD and NetBSD versions (based on online manpages).

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FSEEKO.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FSEEKO.h
@@ -8,13 +8,16 @@
 
 /* This checks for fseeko(), which is a large-file-aware
  * alternative to fseek().
+ *
+ * Since OpenBSD 2.7 (June 2000).
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)    || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0)  || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)   || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)   || \
-    defined(__CYGWIN__)                   || \
-    defined(__MINGW32__)                  || \
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)     || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 2)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(200006) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 6)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)    || \
+    defined(__CYGWIN__)                    || \
+    defined(__MINGW32__)                   || \
     (defined(__sun) && defined(__SVR4))   // Solaris
 #  define HAVE_FSEEKO 1
 #endif
