ee9e65998d9366bca85cacf4e2f1347e753397b2
Francois Kritzinger <francoisk777@gmail.com>
2025-05-13 17:02:20 +0200

    Update HAVE_GETOPT

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETOPT.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETOPT.h
@@ -2,14 +2,15 @@
 
 #undef HAVE_GETOPT
 
-/* Since POSIX.2, 4.3BSD
- * OpenBSD 2.1, FreeBSD 1.0, NetBSD 1.3, glibc 2.0, Solaris
+/* Since POSIX.2, 4.3BSD, and thus in OpenBSD, FreeBSD, NetBSD, glibc, macOS,
+ * Mingw-w64, and Solaris since the beginning.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0) || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199105) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0) || \
+#if defined(__GLIBC__)             || \
+    defined(__FreeBSD__)           || \
+    defined(__NetBSD__)            || \
+    defined(__OpenBSD__)           || \
+    defined(BUILD2_AUTOCONF_MACOS) || \
+    defined(__MINGW32__)           || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_GETOPT 1
 #endif
