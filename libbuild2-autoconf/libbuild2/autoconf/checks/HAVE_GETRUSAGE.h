14134c8d846b34cb7031710cbe257f312963de93
Francois Kritzinger <francoisk777@gmail.com>
2025-05-13 17:29:54 +0200

    Update HAVE_GETRUSAGE

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETRUSAGE.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETRUSAGE.h
@@ -2,13 +2,13 @@
 
 #undef HAVE_GETRUSAGE
 
-/* Since POSIX.1-2001, SVr4, 4.3BSD,
- * OpenBSD 2.1, FreeBSD 1.0, NetBSD 1.3, glibc 2.33, Solaris
+/* Since POSIX.1-2001, SVr4, 4.3BSD, and thus in OpenBSD, FreeBSD, NetBSD,
+ * glibc, macOS, and Solaris since the beginning.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 33)     || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0)  || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199105) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3) || \
+#if defined(__GLIBC__)             || \
+    defined(__FreeBSD__)           || \
+    defined(__NetBSD__)            || \
+    defined(__OpenBSD__)           || \
     defined(BUILD2_AUTOCONF_MACOS) || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_GETRUSAGE 1
