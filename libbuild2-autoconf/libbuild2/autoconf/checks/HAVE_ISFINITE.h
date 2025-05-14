918c832cb76759651a9541d9227dc3df0dc96a1e
Francois Kritzinger <francoisk777@gmail.com>
2025-05-14 16:49:31 +0200

    Update HAVE_ISFINITE
    The macOS version was determined by looking at the libm commit history at
    https://github.com/apple-oss-distributions/Libm and cross-referencing with the
    corresponding macOS versions provided by
    https://github.com/simonbyrne/apple-libm.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ISFINITE.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ISFINITE.h
@@ -6,16 +6,14 @@
 
 #undef HAVE_ISFINITE
 
-/* Since Linux/glibc 2.0, FreeBSD 5.1, OpenBSD 4.4, NetBSD 1.3, Mac OS, Solaris,
- * MingW
+/* Since Linux/glibc 2.0, FreeBSD 5.1, OpenBSD 4.4 (November 2008), NetBSD 2.0
+ * (based on commit history), Mac OS 10.2, Solaris, Mingw-w64.
  */
 #if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)     || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 1)   || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(200910) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3)    || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 6)    || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(1, 0)     || \
-    defined(BUILD2_AUTOCONF_MACOS)         || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(200811) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 2)    || \
     defined(_WIN32)                        || \
     defined(__MINGW32__)                   || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
