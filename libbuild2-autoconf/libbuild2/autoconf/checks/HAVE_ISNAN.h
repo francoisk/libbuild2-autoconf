fcd927f4e90445a80e62ac10f2d939adf01f0fae
Francois Kritzinger <francoisk777@gmail.com>
2025-05-15 09:31:03 +0200

    Update HAVE_ISNAN

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ISNAN.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ISNAN.h
@@ -6,16 +6,16 @@
 
 #undef HAVE_ISNAN
 
-/* Since Linux/glibc 2.0, FreeBSD 5.1, OpenBSD 4.4, NetBSD 1.3, Mac OS, Solaris
+/* Linux/glibc, FreeBSD, OpenBSD, NetBSD, Mac OS 10.2, Windows, Mingw-w64,
+ * Solaris.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)     || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 1)   || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(200910) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3)    || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 6)    || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(1, 0)     || \
-    defined(BUILD2_AUTOCONF_MACOS)         || \
-    defined(_WIN32)                        || \
+#if defined(__GLIBC__)                  || \
+    defined(__FreeBSD__)                || \
+    defined(__NetBSD__)                 || \
+    defined(__OpenBSD__)                || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 2) || \
+    defined(__MINGW32__)                || \
+    defined(_WIN32)                     || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_ISNAN 1
 #endif
