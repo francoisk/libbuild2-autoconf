aa47477825f548af8120f831453cb47d565ea46e
Francois Kritzinger <francoisk777@gmail.com>
2025-05-15 11:24:08 +0200

    Update HAVE_LOCALTIME_R

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_LOCALTIME_R.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_LOCALTIME_R.h
@@ -6,15 +6,15 @@
 
 #undef HAVE_LOCALTIME_R
 
-/*  Since glibc 2.0, 4.3BSD, FreeBSD 3.0, OpenBSD 2.1, NetBSD 1.2, MacOS 10.0, Mingw-w64 2.0
- *  POSIX.1c, SVr4
+/*  Since glibc 2.0, 4.3BSD, FreeBSD 8.0, OpenBSD 2.5 (May 1999), NetBSD 1.4,
+ *  MacOS 10.0, Mingw-w64 2.0 POSIX.1c, SVr4
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0) || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199706) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 2) || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(2, 0) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0) || \
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)     || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(8, 0)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199905) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 4)    || \
+    BUILD2_AUTOCONF_MINGW_PREREQ(2, 0)     || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 2)    || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_LOCALTIME_R 1
 #endif
