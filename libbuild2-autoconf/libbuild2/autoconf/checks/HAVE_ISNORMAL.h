351be2831a6b15f4d48f06f9dc1d8e67d05472da
Francois Kritzinger <francoisk777@gmail.com>
2025-05-15 10:43:57 +0200

    Update HAVE_ISNORMAL

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ISNORMAL.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ISNORMAL.h
@@ -6,16 +6,17 @@
 
 #undef HAVE_ISNORMAL
 
-/* Since Linux/glibc 2.0, FreeBSD 5.1, OpenBSD 4.4, NetBSD 1.3, Mac OS, Solaris
+/* Linux/glibc 2.0, FreeBSD 5.1, OpenBSD 4.4 (November 2008), NetBSD 2.0, Mac
+ * OS 10.2, Windows, Mingw-w64, Solaris.
  */
 #if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)     || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 1)   || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(200910) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3)    || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 6)    || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(1, 0)     || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(200811) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 2)    || \
     defined(BUILD2_AUTOCONF_MACOS)         || \
     defined(_WIN32)                        || \
+    defined(__MINGW32__)                   || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_ISNORMAL 1
 #endif
