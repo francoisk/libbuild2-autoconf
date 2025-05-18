58fed843333812139ded8426e2eb8c185b46f58d
Francois Kritzinger <francoisk777@gmail.com>
2025-05-18 11:03:18 +0200

    Update HAVE_PTHREAD_CANCEL

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_CANCEL.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_CANCEL.h
@@ -6,14 +6,16 @@
 
 #undef HAVE_PTHREAD_CANCEL
 
-/* Presence of the pthread_cancel() function.
- * Since glibc 2.0, FreeBSD 5.2, OpenBSD 3.3, NetBSD 3.0, MacOS 10.0
+/* Presence of the pthread_cancel() function.  Since glibc 2.1, FreeBSD 4.0
+ * (online manpages), OpenBSD 2.5 (May 1999), NetBSD 2.0, MacOS 10.10,
+ * Mingw-w64 3.0.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)    || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 2)  || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(200305) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(3, 0) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0) || \
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)     || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(4, 0)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199905) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 10)   || \
+    BUILD2_AUTOCONF_MINGW_PREREQ(3, 0)     || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_PTHREAD_CANCEL 1
 #endif
