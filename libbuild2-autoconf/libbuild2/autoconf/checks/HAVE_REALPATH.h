35fa744761d57ec6b7b22f5d6c759ca0b47c742b
Francois Kritzinger <francoisk777@gmail.com>
2025-05-18 16:42:55 +0200

    Update HAVE_REALPATH

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_REALPATH.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_REALPATH.h
@@ -7,10 +7,14 @@
 #undef HAVE_REALPATH
 
 /* This checks for the realpath() function, which resolves absolute paths.
+ *
+ * Since glibc 1.90, FreeBSD 2.2.0, NetBSD 1.1, OpenBSD (with initial NetBSD
+ * import), macOS 10.2.
  */
-#if defined(__linux__) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(1, 90)  || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(2, 2) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 1)  || \
+    defined(__OpenBSD__)                 || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 2)
 #  define HAVE_REALPATH 1
 #endif
