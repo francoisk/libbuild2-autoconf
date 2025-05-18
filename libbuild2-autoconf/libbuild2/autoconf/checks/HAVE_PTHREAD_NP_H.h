b044cc13e6f67eca3254d5c7878dc356534ee361
Francois Kritzinger <francoisk777@gmail.com>
2025-05-18 14:49:49 +0200

    Update HAVE_PTHREAD_NP_H

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_NP_H.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_NP_H.h
@@ -6,9 +6,17 @@
 
 #undef HAVE_PTHREAD_NP_H
 
-/* Existence of the pthread_np.h header, which provides non-standard POSIX (NP).
- * FreeBSD 5.0
+/* Existence of the pthread_np.h header, which provides non-portable pthread
+ * functions.
+ *
+ * Since FreeBSD 5.1, OpenBSD 2.4 (released December 1998; manpage was added
+ * in later release).
+ *
+ * TODO HAVE_PTHREAD_SET_NAME_NP is based on this check so confirm that
+ * pthread_np.h declares pthread_set_name_np() on any new platforms added
+ * below.
  */
-#if BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 0)
+#if BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 1) || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199812)
 #  define HAVE_PTHREAD_NP_H 1
 #endif
