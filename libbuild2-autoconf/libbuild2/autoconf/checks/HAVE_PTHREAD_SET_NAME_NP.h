80ce749cf4c97c673d70d2c9b7ddad953d756e13
Francois Kritzinger <francoisk777@gmail.com>
2025-05-18 14:50:03 +0200

    Base HAVE_PTHREAD_SET_NAME_NP on HAVE_PTHREAD_NP_H

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_SET_NAME_NP.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_SET_NAME_NP.h
@@ -1,17 +1,13 @@
-// HAVE_PTHREAD_SET_NAME_NP : BUILD2_AUTOCONF_LIBC_VERSION
-
-#ifndef BUILD2_AUTOCONF_LIBC_VERSION
-#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
-#endif
+// HAVE_PTHREAD_SET_NAME_NP : HAVE_PTHREAD_NP_H
 
 #undef HAVE_PTHREAD_SET_NAME_NP
 
-/* Check for the presence of the pthread_set_name_np() function,
- * which sets the name of a thread. This is available on:
- * - FreeBSD 5.1+ (note that pthread_setname_np() was added later)
- * - OpenBSD 2.7 (released June 2000)
+/* Check for the presence of the pthread_set_name_np() function, which sets
+ * the name of a thread.
+ *
+ * Base this check on HAVE_PTHREAD_NP_H because this function is declared in
+ * pthread_np.h.
  */
-#if BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 1)   || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(200006)
+#ifdef HAVE_PTHREAD_NP_H
 #  define HAVE_PTHREAD_SET_NAME_NP 1
 #endif
