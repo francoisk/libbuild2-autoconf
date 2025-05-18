14f3f96eb29baa2b1104d9e6ceaa76956c3d7a04
Francois Kritzinger <francoisk777@gmail.com>
2025-05-18 15:21:53 +0200

    Update HAVE_ROUND and HAVE_ROUNDF

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ROUND.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ROUND.h
@@ -6,16 +6,16 @@
 
 #undef HAVE_ROUND
 
-/*  Since glibc 2.1, 4.3BSD, FreeBSD 1.0, OpenBSD 2.0, NetBSD 1.0, MacOS, Mingw-w64 1.0
- *  POSIX.1, SVr4
+/*  Since glibc 2.1, 4.3BSD, FreeBSD 5.3, OpenBSD 4.0 (November 2006), NetBSD
+ *  2.0, MacOS 10.2, Windows, Mingw-w64, POSIX.1, SVr4
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0) || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199510) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 0) || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(1, 0) || \
-    defined(BUILD2_AUTOCONF_MACOS) || \
-    defined(_WIN32) || \
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)     || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 3)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(200611) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 2)    || \
+    defined(_WIN32)                        || \
+    defined(__MINGW32__)                   || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_ROUND 1
 #endif
