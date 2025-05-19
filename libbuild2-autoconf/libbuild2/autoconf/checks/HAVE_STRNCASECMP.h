a0d5a5a4651d98ffa7029b52620b8814993be779
Francois Kritzinger <francoisk777@gmail.com>
2025-05-19 14:58:29 +0200

    Update HAVE_STRNCASECMP

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRNCASECMP.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRNCASECMP.h
@@ -1,8 +1,4 @@
-// HAVE_STRNCASECMP : BUILD2_AUTOCONF_LIBC_VERSION
-
-#ifndef BUILD2_AUTOCONF_LIBC_VERSION
-#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
-#endif
+// HAVE_STRNCASECMP : HAVE_STRCASECMP
 
 #undef HAVE_STRNCASECMP
 
@@ -10,11 +6,10 @@
  * which performs a case-insensitive comparison of two strings up
  * to a specified number of characters.
  * It is part of the POSIX standard and is widely available on Unix-like systems.
+ *
+ * strncasecmp() and strcasecmp() were added at the same time in all of the
+ * systems I checked (see HAVE_STRCASECMP.h).
  */
-#if defined(__linux__) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 0) || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199706) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)
+#ifdef HAVE_STRCASECMP
 #  define HAVE_STRNCASECMP 1
 #endif
