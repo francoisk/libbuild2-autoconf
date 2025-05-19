9ff47a480aaf81c92ac534543702ce3d78f62ed8
Francois Kritzinger <francoisk777@gmail.com>
2025-05-19 11:45:00 +0200

    Update HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC.h
@@ -8,11 +8,11 @@
 
 /* Presence of the st_mtim.tv_nsec field in the stat structure,
  * which provides nanosecond resolution for file modification times.
- * Since glibc 2.12, FreeBSD 5.0, OpenBSD 5.6, NetBSD 6.0, MacOS 10.5
+ * Since glibc 2.12, FreeBSD 9.0, OpenBSD 2.0 (October 1996), NetBSD 7.0.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 12) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 0)  || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(201411) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(6, 0)
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 12)    || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(9, 0)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199610) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(7, 0)
 #  define HAVE_STRUCT_STAT_ST_MTIM_TV_NSEC 1
 #endif
