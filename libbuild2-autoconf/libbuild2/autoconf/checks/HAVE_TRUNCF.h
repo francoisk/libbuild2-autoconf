16c9358d189dd8804bc40e63fd4fa6071b7b1741
Francois Kritzinger <francoisk777@gmail.com>
2025-05-20 09:13:18 +0200

    Update HAVE_TRUNC and HAVE_TRUNCF

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_TRUNCF.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_TRUNCF.h
@@ -1,18 +1,12 @@
-// HAVE_TRUNCF : BUILD2_AUTOCONF_LIBC_VERSION
-
-#ifndef BUILD2_AUTOCONF_LIBC_VERSION
-#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
-#endif
+// HAVE_TRUNCF : HAVE_TRUNC
 
 #undef HAVE_TRUNCF
 
-/* Since glibc 2.1, FreeBSD 5.3, OpenBSD 4.0, NetBSD 4.0 and MacOS, Win32.
+/* Checks for the truncf() function from <math.h>.
+ *
+ * trunc() and truncf() were added at the same time in all of the systems I
+ * checked (see HAVE_TRUNC.h)
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)     || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 3)   || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(200611) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(4, 0)    || \
-    defined(BUILD2_AUTOCONF_MACOS)         || \
-    defined(_WIN32)
+#ifdef HAVE_TRUNC
 #  define HAVE_TRUNCF 1
 #endif
