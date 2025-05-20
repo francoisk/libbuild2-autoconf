ace2b3d55c1ba09f9cfad75987a80649792155c0
Francois Kritzinger <francoisk777@gmail.com>
2025-05-20 11:15:18 +0200

    Update HAVE_SOCKLEN_T

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SOCKLEN_T.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SOCKLEN_T.h
@@ -7,16 +7,22 @@
 #undef HAVE_SOCKLEN_T
 
 /* socklen_t is available in:
- * - glibc since 2.0
+ * - glibc since 2.0.x (so round up to 2.1)
  * - BSD systems (including MacOS)
- * - Solaris, Cygwin
+ * - OpenBSD since 2.5 (May 1999)
+ * - Windows, Mingw-w64, Cygwin, Solaris
+ *
+ * On Windows the winsock2 header ws2tcpip.h defines socklen_t. Mingw-w64 has
+ * had socklen_t (also in ws2tcpip.h) since v1.0.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(4, 0) || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199510) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 0) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0) || \
-    (defined(__sun) && defined(__SVR4)) || \
-    defined(__CYGWIN__)
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)     || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(4, 0)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199905) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 4)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 3)    || \
+    defined(_WIN32)                        || \
+    defined(__MINGW32__)                   || \
+    defined(__CYGWIN__)                    || \
+    (defined(__sun) && defined(__SVR4))
 #  define HAVE_SOCKLEN_T 1
 #endif
