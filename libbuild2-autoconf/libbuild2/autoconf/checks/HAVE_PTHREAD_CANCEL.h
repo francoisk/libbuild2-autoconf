2f112ff34563296f35fc0376bf6273f6aacfc8fb
Francois Kritzinger <francoisk777@gmail.com>
2025-05-21 15:48:06 +0200

    Update HAVE_PTHREAD_CANCEL (remove Mingw-w64)

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_CANCEL.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_PTHREAD_CANCEL.h
@@ -7,15 +7,15 @@
 #undef HAVE_PTHREAD_CANCEL
 
 /* Presence of the pthread_cancel() function.  Since glibc 2.1, FreeBSD 4.0
- * (online manpages), OpenBSD 2.5 (May 1999), NetBSD 2.0, MacOS 10.10,
- * Mingw-w64 3.0.
+ * (online manpages), OpenBSD 2.5 (May 1999), NetBSD 2.0, and MacOS 10.10.
+ *
+ * Note: see also HAVE_PTHREAD (especially with regards to Mingw-w64).
  */
 #if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)     || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(4, 0)   || \
     BUILD2_AUTOCONF_OPENBSD_PREREQ(199905) || \
     BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
     BUILD2_AUTOCONF_MACOS_PREREQ(10, 10)   || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(3, 0)     || \
     ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_PTHREAD_CANCEL 1
 #endif
