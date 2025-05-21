b272a7d974f125a62a49179c8aadb6f21b855a53
Francois Kritzinger <francoisk777@gmail.com>
2025-05-21 09:44:53 +0200

    Update restrict.h
    - Don't define restrict as a macro if in >=C99 mode
    
      The keyword should be available and reserved keywords are not supposed to be
      redefined.
    
    - Define restrict to empty if there is no variant spelling
    
    This emulates the AC_C_RESTRICT GNU autoconf macro.

--- libbuild2-autoconf/libbuild2/autoconf/checks/restrict.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/restrict.h
@@ -1,26 +1,30 @@
 // restrict!
 
-/* If restrict is already defined, assume it's correct and use it as-is.
+/* Ensure restrict is available. This is a type qualifier that informs the
+ * compiler that a pointer is the sole reference to a memory location,
+ * allowing for optimization. If restrict is not natively supported, an
+ * alternative definition may be provided.
  *
  * Note that we may not be able to redefine it because while we can undef the
  * macro, there is no guarantee we will be able to re-include the header (due
  * to include guards).
- *
- * Determines whether the C99 restrict keyword is supported by the compiler.
- * The restrict keyword is a type qualifier that informs the compiler that
- * a pointer is the sole reference to a memory location, allowing for
- * optimization. If restrict is not natively supported, an alternative
- * definition may be provided.
+ */
+
+/* If restrict is already defined as a macro, assume it's correct and use it
+ * as-is.
  */
 #ifndef restrict
-  /* POSIX and MinGW (which also has <sys/types.h> that defines restrict).
-   */
-/* Check for C99 or compiler-specific support for `restrict`. */
-# if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
-#   define restrict restrict
-# elif defined(__GNUC__) || defined(__clang__)
-#   define restrict __restrict__
-# elif defined(_MSC_VER)
-#   define restrict __restrict
-# endif
+/* If in C99 mode or newer, restrict is a keyword so do nothing. Otherwise, if
+ * the compiler supports a variant spelling, define restrict to that.
+ * Otherwise define restrict to empty.
+ */
+# if !defined(__STDC_VERSION__) || (__STDC_VERSION__ < 199901L)
+#   if defined(__GNUC__) || defined(__clang__)
+#     define restrict __restrict__
+#   elif defined(_MSC_VER)
+#     define restrict __restrict
+#   else
+#     define restrict
+#   endif
+#  endif
 #endif
